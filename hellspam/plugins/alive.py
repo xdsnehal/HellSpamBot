a4758cde6fd22ee75c204a8d9de1214878d4ebda
Akhil <98938102+akhilprs@users.noreply.github.com>
2022-08-30 19:17:57 +0530

    Update alive.py

--- hellspam/plugins/alive.py
+++ hellspam/plugins/alive.py
@@ -12,12 +12,12 @@ master = f"[{OWNER_NAME}](tg://user?id={OWNER_ID})"
 
 alive_msg = f"""
 {BIO_MSG}
-☆━━━━━━━━━━━━━━━━☆ 
+☆━━━━━━━━━━━━━☆ 
 ✪ Master:- {master}
 ✪ Bot Version:- `{BOT_VERSION}`
-✪ Creator:- [Akhil](tg://user?id={2102783671}
+✪ Creator:- [Akhil](tg://user?id={2102783671})
 ✪ Telethon Version:- `{version.__version__}`
-☆━━━━━━━━━━━━━━━━☆ 
+☆━━━━━━━━━━━━━☆ 
 © @HellSpamBot
 """
 
