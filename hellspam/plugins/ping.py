c002e621c79035e76062b063f17f8fbc4a0c4f16
xdsnehal <114375185+xdsnehal@users.noreply.github.com>
2022-09-26 11:15:57 +0530

    Update ping.py

--- hellspam/plugins/ping.py
+++ hellspam/plugins/ping.py
@@ -6,15 +6,16 @@ from hellspam import SpamBot1, SpamBot2, SpamBot3, SpamBot4, SpamBot5
 from telethon import events
 from datetime import datetime
 
-@SpamBot1.on(events.NewMessage(incoming=True, pattern='/ping'))
-@SpamBot2.on(events.NewMessage(incoming=True, pattern='/ping'))
-@SpamBot3.on(events.NewMessage(incoming=True, pattern='/ping'))
-@SpamBot4.on(events.NewMessage(incoming=True, pattern='/ping'))
-@SpamBot5.on(events.NewMessage(incoming=True, pattern='/ping'))
+@SpamBot1.on(events.NewMessage(incoming=True, pattern='!ping'))
+@SpamBot2.on(events.NewMessage(incoming=True, pattern='!ping'))
+@SpamBot3.on(events.NewMessage(incoming=True, pattern='!ping'))
+@SpamBot4.on(events.NewMessage(incoming=True, pattern='!ping'))
+@SpamBot5.on(events.NewMessage(incoming=True, pattern='!ping'))
 async def ping(e):
     if e.sender_id in MY_USERS:
         before = datetime.now()
         message = await e.client.send_message(e.chat_id, "`sabar rhak lodu..!`")
         after = datetime.now()
         ms = (after - before).microseconds / 1000
-        await e.client.edit_message(message, f"URANIUM HU.!\n\n✪ URANIUM Spam Bot\n\n PANGA NHI LENA:- [@aa_raha_hu_ruk](tg://user?id={OWNER_ID})\n\n DHEK LODE TU :- {ms} ms\n\nURANIM Spam Bot On Fire 🔥")
+        await e.client.edit_message(message, f"𝗨𝗥𝗔𝗡𝗜𝗨𝗠 ✘ 𝗦𝗣𝗔𝗠 .......
+𝗧𝗨𝗠𝗛𝗔𝗥𝗘 𝗠𝗔𝗔 𝗞𝗔 𝗕𝗢𝗫𝗗𝗔𝗔𝗔👅🤣 ")
